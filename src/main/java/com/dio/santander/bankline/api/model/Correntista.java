54a10e3fd840b14f3cfe17a011cc594df82a287d
Marlus-Sabino <marlussabino@gmail.com>
2022-05-03 17:36:35 +0100

    Add the api-bankline-model and its classes 	- Class conta: (numero,Saldo) 	- Class Correntistas: (Id, CPF, nome) 	- Class Movimentacao: (Id, dataHora, descricao, valor) 	- Class MovimentacaoTipo: (enum{Receita,Despesa})

---
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -0,0 +1,36 @@
+package com.dio.santander.bankline.api.model;
+
+public class Correntista {
+	private Integer Id;
+	private String CPF;
+	private String nome;
+	
+	private Conta conta;
+	
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
