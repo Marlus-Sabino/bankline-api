461942b17420856772ac4531284afb1bd1dd478f
Marlus-Sabino <marlussabino@gmail.com>
2022-05-04 06:41:51 +0100

    Final Version of the API-DIO classroom

--- src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dio.santander.bankline.api.dto.NovaMovimentacao;
+import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.model.Movimentacao;
 import com.dio.santander.bankline.api.model.MovimentacaoTipo;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
 
 @Service
 public class MovimentacaoService {
 	@Autowired
-	
 	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
 	
 	public void save(NovaMovimentacao novaMovimentacao) {
 		Movimentacao movimentacao = new Movimentacao();
@@ -27,6 +31,11 @@ public class MovimentacaoService {
 		movimentacao.setTipo(movimentacao.getTipo());
 		movimentacao.setValor(valor);
 		
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if(correntista != null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			correntistaRepository.save(correntista);
+		}
 		repository.save(movimentacao);
 	}
 	
